50e026ecbfb439988dd829c7738b8cb6ecd73046
Laird Nelson <ljnelson@gmail.com>
2020-12-01 15:36:07 -0800

    Squashable commit; completed constructors in JerseyChannelInitializer
    Signed-off-by: Laird Nelson <ljnelson@gmail.com>

--- src/main/java/org/microbean/jersey/netty/JerseyChannelInitializer.java
+++ src/main/java/org/microbean/jersey/netty/JerseyChannelInitializer.java
@@ -730,6 +730,106 @@ public class JerseyChannelInitializer extends ChannelInitializer<Channel> {
    * be {@code null} in which case a new {@link
    * DefaultEventExecutorGroup} will be used instead
    *
+   * @param applicationHandlerSupplier a {@link Supplier} of an {@link
+   * ApplicationHandler} representing a <a
+   * href="https://jakarta.ee/specifications/restful-ws/"
+   * target="_parent">Jakarta RESTful Web Services application</a>
+   * whose {@link ApplicationHandler#handle(ContainerRequest)} method
+   * will serve as the bridge between Netty and Jersey; may be {@code
+   * null} somewhat pathologically but normally is not
+   *
+   * @param flushThreshold the minimum number of bytes that an {@link
+   * AbstractByteBufBackedChannelOutboundInvokingOutputStream}
+   * returned by an implementation of the {@link
+   * AbstractContainerRequestHandlingResponseWriter#createOutputStream(long,
+   * ContainerResponse)} method must write before an automatic
+   * {@linkplain
+   * AbstractByteBufBackedChannelOutboundInvokingOutputStream#flush()
+   * flush} may take place; if less than {@code 0} {@code 0} will be
+   * used instead; if {@link Integer#MAX_VALUE} then it is suggested
+   * that no automatic flushing will occur
+   *
+   * @param byteBufCreator a {@link ByteBufCreator} that will be
+   * {@linkplain
+   * AbstractContainerRequestHandlingResponseWriter#AbstractContainerRequestHandlingResponseWriter(Supplier,
+   * int,
+   * AbstractByteBufBackedChannelOutboundInvokingOutputStream.ByteBufCreator)
+   * passed to an
+   * <code>AbstractContainerRequestHandlingResponseWriter</code>}
+   * implementation; may be {@code null}
+   *
+   * @see #JerseyChannelInitializer(URI, SslContext, boolean, long,
+   * EventExecutorGroup, boolean, Supplier, int,
+   * AbstractByteBufBackedChannelOutboundInvokingOutputStream.ByteBufCreator)
+   * @see ContainerRequest
+   *
+   * @see SslContext
+   *
+   * @see
+   * HttpServerUpgradeHandler#HttpServerUpgradeHandler(SourceCodec,
+   * UpgradeCodecFactory, int)
+   *
+   * @see ApplicationHandler#handle(ContainerRequest)
+   *
+   * @see AbstractContainerRequestHandlingResponseWriter
+   *
+   * @see HttpObjectToContainerRequestDecoder
+   *
+   * @see HttpContainerRequestHandlingResponseWriter
+   *
+   * @see Http2StreamFrameToContainerRequestDecoder
+   *
+   * @see Http2ContainerRequestHandlingResponseWriter
+   */
+  public JerseyChannelInitializer(final URI baseUri,
+                                  final SslContext sslContext,
+                                  final boolean http2Support,
+                                  final long maxIncomingContentLength,
+                                  final EventExecutorGroup jerseyEventExecutorGroup,
+                                  final Supplier<? extends ApplicationHandler> applicationHandlerSupplier,
+                                  final int flushThreshold,
+                                  final ByteBufCreator byteBufCreator) {
+    this(baseUri,
+         sslContext,
+         http2Support,
+         maxIncomingContentLength,
+         jerseyEventExecutorGroup,
+         true, /* use Jersey injection */
+         applicationHandlerSupplier,
+         flushThreshold,
+         byteBufCreator);
+  }
+
+  /**
+   * Creates a new {@link JerseyChannelInitializer}.
+   *
+   * @param baseUri a {@link URI} that will serve as the {@linkplain
+   * ContainerRequest#getBaseUri() base <code>URI</code>} in a new
+   * {@link ContainerRequest}; may be {@code null} in which case the
+   * return value of {@link URI#create(String) URI.create("/")} will
+   * be used instead
+   *
+   * @param sslContext an {@link SslContext}; may be {@code null} in
+   * which case network communications will occur in plain text
+   *
+   * @param http2Support if HTTP/2 support (including upgrades, prior
+   * knowledge, h2c, etc.) should be enabled
+   *
+   * @param maxIncomingContentLength in the case of HTTP to HTTP/2
+   * upgrades, a {@code long} that governs the maximum permitted
+   * incoming entity length in bytes; if less than {@code 0} then
+   * {@link Long#MAX_VALUE} will be used instead; if exactly {@code 0}
+   * then if the HTTP message containing the upgrade header is
+   * something like a {@code POST} it will be rejected with a {@code
+   * 413} error code; ignored entirely if {@code http2Support} is
+   * {@code false}
+   *
+   * @param jerseyEventExecutorGroup an {@link EventExecutorGroup}
+   * that will manage the thread on which an {@link
+   * ApplicationHandler#handle(ContainerRequest)} call will occur; may
+   * be {@code null} in which case a new {@link
+   * DefaultEventExecutorGroup} will be used instead
+   *
    * @param useJerseyInjection if {@code true} then certain Netty
    * constructs like {@link ChannelHandlerContext} will be made
    * available for dependency injection in user applications using
